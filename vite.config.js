921f2d0b2a79cf1d5416c02f44c5fb4075a6da31
Scotny <marsithb2022@gmail.com>
2025-04-21 20:30:29 +0500

    compatible with vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/9m-3l/',  // Add this line, with your repo name
+  base: process.env.NODE_ENV === 'production' ? '/9m-3l/' : '/',
 });
 
